2589d820af45f8e251444619636881c729ed6af4
Sclarke5 <sclarke5555@gmail.com>
2018-05-07 18:55:46 -0400

    adds csv, but check with instructor whether correct--doesn't display within browser

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,10 @@ class BooksController < ApplicationController
     respond_to do |wants|
       wants.html
       wants.text
+
+      wants.csv do
+        render plain: Book.generate_csv(@books)
+      end
     end
   end
 
